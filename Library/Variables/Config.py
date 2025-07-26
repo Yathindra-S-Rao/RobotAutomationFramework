f6d222575bf146c6611252ef9127d8e7c0f33b39
Yathindra-S-Rao <syathindrarao@gmail.com>
2025-07-26 19:31:31 +0530

    run_test.py is included with parallel execution and number of processes can be configured using Config.py

---
+++ Library/Variables/Config.py
@@ -0,0 +1,3 @@
+# Define number of process to be executed parallelly
+
+PROCESS_COUNT = 4
--- run_tests.py
+++ run_tests.py
@@ -2,16 +2,19 @@ import argparse
 import datetime
 import subprocess
 import os
+from Library.Variables.Config import PROCESS_COUNT
 
 
-def run_robot_test(test_suite):
+def run_robot_test(test_suite, processes):
+    print('Process Count = '+str(PROCESS_COUNT))
     suite_name = (test_suite.split('/'))[-1].lower()
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     output_dir = os.path.abspath("Results")         # Absolute Directory
     os.makedirs(output_dir, exist_ok=True)          # Create if directory does not exists
 
     robot_command = [
-        'robot',
+        'pabot',
+        '--processes', str(processes),
         '--outputdir', output_dir,
         '--log', f'log_{suite_name}_{timestamp}.html',
         '--report', f'report_{suite_name}_{timestamp}.html',
@@ -33,9 +36,16 @@ def parse_argument():
         help='Directory containing Robot Framework test suite'
     )
 
+    parser.add_argument(
+        '--processes',
+        type=int,
+        default=PROCESS_COUNT,
+        help='Number of processes that runs parallaly'
+    )
+
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_argument()
-    run_robot_test(args.suite)
+    run_robot_test(args.suite, args.processes)
